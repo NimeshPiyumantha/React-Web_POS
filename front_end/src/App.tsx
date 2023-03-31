0fb33906a4dde9ab4cd7a5b8c63924eb7da81b80
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-03-31 15:05:41 +0530

    Update react project

--- front_end/src/App.tsx
+++ front_end/src/App.tsx
@@ -1,14 +1,14 @@
-import React, { Component } from 'react';
-import './App.css';
-import Home from './pages/Home/Home';
-import Header from './component/Header/Header';
+import React, { Component } from "react";
+import "./App.css";
+import Home from "./pages/Home/Home";
+import Header from "./component/Header/Header";
 
 export default class App extends Component {
   render(): React.ReactNode {
     return (
       <div className="App">
-        <Header/>
-      <Home/>
+        <Header />
+        <Home />
       </div>
     );
   }
