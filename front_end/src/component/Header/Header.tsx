55fef4231cad9d53336c43df0735034a6777e7af
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-03-31 15:09:58 +0530

    Update react project

--- front_end/src/component/Header/Header.tsx
+++ front_end/src/component/Header/Header.tsx
@@ -27,6 +27,10 @@ function Header() {
   const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
     setAnchorElNav(event.currentTarget);
   };
+  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
 
 
   return (
