55f26fed9ee4c5e40b32ba9fd03f129da6e42e56
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-03-31 15:13:46 +0530

    Update react project

--- front_end/src/component/Header/Header.tsx
+++ front_end/src/component/Header/Header.tsx
@@ -117,6 +117,17 @@ function Header() {
           >
             LOGO
           </Typography>
+          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+            {pages.map((page) => (
+              <Button
+                key={page}
+                onClick={handleCloseNavMenu}
+                sx={{ my: 2, color: "white", display: "block" }}
+              >
+                {page}
+              </Button>
+            ))}
+          </Box>
         </Toolbar>
       </Container>
     </AppBar>
