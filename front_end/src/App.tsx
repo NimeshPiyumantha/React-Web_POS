0fb530c4b7bdfcb6c8f18a1f0ee7054bff294a1e
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-03-31 09:08:26 +0530

    Remove contents

--- front_end/src/App.tsx
+++ front_end/src/App.tsx
@@ -2,12 +2,10 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+export default class App  {
+  render(): React.ReactNode {
+    return (
+      <div className="App"></div>
+    );
+  }
 }
-
-export default App;
