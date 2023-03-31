12b896f4802dfa6c6cec826d7ea3d633d5c96813
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-03-31 15:13:25 +0530

    Add logo

--- front_end/src/component/Header/Header.tsx
+++ front_end/src/component/Header/Header.tsx
@@ -99,6 +99,24 @@ function Header() {
             </Menu>
           </Box>
           <AdbIcon sx={{ display: { xs: "flex", md: "none" }, mr: 1 }} />
+          <Typography
+            variant="h5"
+            noWrap
+            component="a"
+            href=""
+            sx={{
+              mr: 2,
+              display: { xs: "flex", md: "none" },
+              flexGrow: 1,
+              fontFamily: "monospace",
+              fontWeight: 700,
+              letterSpacing: ".3rem",
+              color: "inherit",
+              textDecoration: "none",
+            }}
+          >
+            LOGO
+          </Typography>
         </Toolbar>
       </Container>
     </AppBar>
