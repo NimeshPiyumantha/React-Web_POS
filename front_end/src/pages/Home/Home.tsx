b534e66dc3c59cf2a430998a7067d4ceaa5bef2f
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-04-03 10:50:39 +0530

    Update Content

--- front_end/src/pages/Home/Home.tsx
+++ front_end/src/pages/Home/Home.tsx
@@ -69,6 +69,52 @@ export default function Home() {
             </Box>
           </ThemeProvider>
         </Grid>
+        <Grid lg={3} md={3} sm={6} xs={12}>
+          <ThemeProvider theme={theme}>
+            <Box
+              sx={{
+                bgcolor: "background.paper",
+                boxShadow: 2,
+                borderRadius: 2,
+                p: 2,
+                minWidth: 180,
+              }}
+            >
+              <Box sx={{ color: "text.secondary" }}>Item Count</Box>
+              <Box
+                id="txtItemsCount"
+                sx={{
+                  color: "text.primary",
+                  fontSize: 34,
+                  fontWeight: "medium",
+                }}
+              >
+                0
+              </Box>
+              <Box
+                id="txtItemsCountPracentage"
+                sx={{
+                  color: "success.dark",
+                  display: "inline",
+                  fontWeight: "bold",
+                  mx: 0.5,
+                  fontSize: 14,
+                }}
+              >
+                +18.77%
+              </Box>
+              <Box
+                sx={{
+                  color: "text.secondary",
+                  display: "inline",
+                  fontSize: 14,
+                }}
+              >
+                vs. last week
+              </Box>
+            </Box>
+          </ThemeProvider>
+        </Grid>
       </Grid>
     </Box>
   );
