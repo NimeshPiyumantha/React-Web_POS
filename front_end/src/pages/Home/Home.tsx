bbac7e96859f279d6f0d95c4b6853bc206912b1a
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-04-03 08:58:56 +0530

    Update Content

--- front_end/src/pages/Home/Home.tsx
+++ front_end/src/pages/Home/Home.tsx
@@ -1,32 +1,203 @@
 import React from "react";
-import { styled } from "@mui/material/styles";
+import { ThemeProvider, createTheme, styled } from "@mui/material/styles";
 import Paper from "@mui/material/Paper";
 import Grid from "@mui/material/Unstable_Grid2";
 import Box from "@mui/material/Box";
 
-const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
-  ...theme.typography.body2,
-  padding: theme.spacing(3),
-  textAlign: "center",
-  color: theme.palette.text.secondary,
-}));
+const theme = createTheme({
+  palette: {
+    background: {
+      paper: "#fff",
+    },
+    text: {
+      primary: "#173A5E",
+      secondary: "#46505A",
+    },
+    action: {
+      active: "#001E3C",
+    },
+  },
+});
 
 export default function Home() {
   return (
     <Box sx={{ width: "100%" }} mt={5}>
       <Grid container margin={3} rowSpacing={2} columnSpacing={3}>
         <Grid lg={3} md={3} sm={6} xs={12}>
-          <Item>1</Item>
+          <ThemeProvider theme={theme}>
+            <Box
+              sx={{
+                bgcolor: "background.paper",
+                boxShadow: 1,
+                borderRadius: 2,
+                p: 2,
+                minWidth: 180,
+              }}
+            >
+              <Box sx={{ color: "text.secondary" }}>Sessions</Box>
+              <Box
+                sx={{
+                  color: "text.primary",
+                  fontSize: 34,
+                  fontWeight: "medium",
+                }}
+              >
+                98.3 K
+              </Box>
+              <Box
+                sx={{
+                  color: "success.dark",
+                  display: "inline",
+                  fontWeight: "bold",
+                  mx: 0.5,
+                  fontSize: 14,
+                }}
+              >
+                +18.77%
+              </Box>
+              <Box
+                sx={{
+                  color: "text.secondary",
+                  display: "inline",
+                  fontSize: 14,
+                }}
+              >
+                vs. last week
+              </Box>
+            </Box>
+          </ThemeProvider>
         </Grid>
         <Grid lg={3} md={3} sm={6} xs={12}>
-          <Item>2</Item>
+          <ThemeProvider theme={theme}>
+            <Box
+              sx={{
+                bgcolor: "background.paper",
+                boxShadow: 1,
+                borderRadius: 2,
+                p: 2,
+                minWidth: 180,
+              }}
+            >
+              <Box sx={{ color: "text.secondary" }}>Sessions</Box>
+              <Box
+                sx={{
+                  color: "text.primary",
+                  fontSize: 34,
+                  fontWeight: "medium",
+                }}
+              >
+                98.3 K
+              </Box>
+              <Box
+                sx={{
+                  color: "success.dark",
+                  display: "inline",
+                  fontWeight: "bold",
+                  mx: 0.5,
+                  fontSize: 14,
+                }}
+              >
+                +18.77%
+              </Box>
+              <Box
+                sx={{
+                  color: "text.secondary",
+                  display: "inline",
+                  fontSize: 14,
+                }}
+              >
+                vs. last week
+              </Box>
+            </Box>
+          </ThemeProvider>
         </Grid>
         <Grid lg={3} md={3} sm={6} xs={12}>
-          <Item>3</Item>
+          <ThemeProvider theme={theme}>
+            <Box
+              sx={{
+                bgcolor: "background.paper",
+                boxShadow: 1,
+                borderRadius: 2,
+                p: 2,
+                minWidth: 180,
+              }}
+            >
+              <Box sx={{ color: "text.secondary" }}>Sessions</Box>
+              <Box
+                sx={{
+                  color: "text.primary",
+                  fontSize: 34,
+                  fontWeight: "medium",
+                }}
+              >
+                98.3 K
+              </Box>
+              <Box
+                sx={{
+                  color: "success.dark",
+                  display: "inline",
+                  fontWeight: "bold",
+                  mx: 0.5,
+                  fontSize: 14,
+                }}
+              >
+                +18.77%
+              </Box>
+              <Box
+                sx={{
+                  color: "text.secondary",
+                  display: "inline",
+                  fontSize: 14,
+                }}
+              >
+                vs. last week
+              </Box>
+            </Box>
+          </ThemeProvider>
         </Grid>
         <Grid lg={3} md={3} sm={6} xs={12}>
-          <Item>4</Item>
+          <ThemeProvider theme={theme}>
+            <Box
+              sx={{
+                bgcolor: "background.paper",
+                boxShadow: 1,
+                borderRadius: 2,
+                p: 2,
+                minWidth: 180,
+              }}
+            >
+              <Box sx={{ color: "text.secondary" }}>Sessions</Box>
+              <Box
+                sx={{
+                  color: "text.primary",
+                  fontSize: 34,
+                  fontWeight: "medium",
+                }}
+              >
+                98.3 K
+              </Box>
+              <Box
+                sx={{
+                  color: "success.dark",
+                  display: "inline",
+                  fontWeight: "bold",
+                  mx: 0.5,
+                  fontSize: 14,
+                }}
+              >
+                +18.77%
+              </Box>
+              <Box
+                sx={{
+                  color: "text.secondary",
+                  display: "inline",
+                  fontSize: 14,
+                }}
+              >
+                vs. last week
+              </Box>
+            </Box>
+          </ThemeProvider>
         </Grid>
       </Grid>
     </Box>
