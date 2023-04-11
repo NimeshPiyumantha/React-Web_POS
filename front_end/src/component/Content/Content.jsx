e8e1f45b565762a888c857a321591e16af4fbd7a
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-04-11 20:47:56 +0530

    Update Content

--- front_end/src/component/Content/Content.jsx
+++ front_end/src/component/Content/Content.jsx
@@ -8,14 +8,14 @@ import { Route, Routes } from "react-router-dom";
 export default function Content() {
   return (
     <div className="mt-4 text-center">
-    <Routes>
-      <Route path="/home" element={<Home />}></Route>
-      <Route path="/customer" element={<Customer />}></Route>
-      <Route path="/item" element={<Item />}></Route>
-      <Route path="/place" element={<PlaceOrder />}></Route>
-      <Route path="/orderDetail" element={<OrderDetails/>}></Route>
-      {/* <Route path="/" element={<Login/>}></Route> */}
-    </Routes>
-  </div>
+      <Routes>
+        <Route path="/home" element={<Home />}></Route>
+        <Route path="/customer" element={<Customer />}></Route>
+        <Route path="/item" element={<Item />}></Route>
+        <Route path="/place" element={<PlaceOrder />}></Route>
+        <Route path="/orderDetail" element={<OrderDetails />}></Route>
+        <Route path="/" element={<Home />}></Route>
+      </Routes>
+    </div>
   );
 }
