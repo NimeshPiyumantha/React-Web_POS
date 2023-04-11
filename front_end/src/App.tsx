19812e894332617867320e1b79bd77e778c26e52
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-04-11 19:43:17 +0530

    Update Content

--- front_end/src/App.tsx
+++ front_end/src/App.tsx
@@ -2,13 +2,13 @@ import React, { Component } from "react";
 import "./App.css";
 import Header from "./component/Header/Header";
 import Home from "./pages/Home/Home";
+import Content from "./component/Content/Content";
 
 export default class App extends Component {
   render(): React.ReactNode {
     return (
       <div className="App">
-        <Header />
-        <Home />
+        <Content/>
       </div>
     );
   }
