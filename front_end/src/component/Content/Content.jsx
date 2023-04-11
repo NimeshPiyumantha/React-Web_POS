d45e7982d98644488e918aa296369250dcf80ab9
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-04-11 19:37:21 +0530

    Update Content

--- front_end/src/component/Content/Content.jsx
+++ front_end/src/component/Content/Content.jsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import React from "react";
+import Home from "../../pages/Home/Home";
+import Customer from "../../pages/Customer/Customer";
+import Item from "../../pages/Item/Item";
+import PlaceOrder from "../../pages/PlaceOrder/PlaceOrder";
+import OrderDetails from "../../pages/OrderDetails/OrderDetails";
 
 export default function Content() {
   return (
-    <div>Content</div>
-  )
+    <div className="mt-4 text-center">
+    <Routes>
+      <Route path="/home" element={<Home />}></Route>
+      <Route path="/customer" element={<Customer />}></Route>
+      <Route path="/item" element={<Item />}></Route>
+      <Route path="/place" element={<PlaceOrder />}></Route>
+      <Route path="/orderDetail" element={<OrderDetails/>}></Route>
+      {/* <Route path="/" element={<Login/>}></Route> */}
+    </Routes>
+  </div>
+  );
 }
