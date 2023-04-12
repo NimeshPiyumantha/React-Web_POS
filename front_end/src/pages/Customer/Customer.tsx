eafb2c070bdd345657934e1a94ea7bb6f36ec799
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-04-12 12:10:05 +0530

    Update Content

--- front_end/src/pages/Customer/Customer.tsx
+++ front_end/src/pages/Customer/Customer.tsx
@@ -1,11 +1,46 @@
 import Box from "@mui/material/Box";
 import Header from "../../component/Header/Header";
+import { Grid, TextField, ThemeProvider, createTheme } from "@mui/material";
+
+const theme = createTheme({
+  palette: {
+    background: {
+      paper: "#fff",
+    },
+    text: {
+      primary: "#173A5E",
+      secondary: "#46505A",
+    },
+    action: {
+      active: "#001E3C",
+    },
+  },
+});
 
 export default function Customer() {
   return (
     <>
       <Header />
-      <Box sx={{ width: "100%" }} mt={5}></Box>
+      <Box sx={{ width: "100%" }} mt={5}>
+        <Grid container spacing={5} margin={3}>
+          <Grid lg={3} md={3} sm={6} xs={12}>
+            <Box
+              component="form"
+              sx={{
+                "& > :not(style)": { m: 1, width: "25ch" },
+              }}
+              noValidate
+              autoComplete="off"
+            >
+              <TextField
+                id="outlined-basic"
+                label="Outlined"
+                variant="outlined"
+              />
+            </Box>
+          </Grid>
+        </Grid>
+      </Box>
     </>
   );
 }
