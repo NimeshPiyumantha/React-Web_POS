1d277a39078386fc103b2d3e3de269fca3858abb
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-04-12 13:33:45 +0530

    Update Content

--- front_end/src/pages/Customer/Customer.tsx
+++ front_end/src/pages/Customer/Customer.tsx
@@ -13,7 +13,7 @@ const theme = createTheme({
     },
     action: {
       active: "#001E3C",
-    }
+    },
   },
 });
 
@@ -31,36 +31,52 @@ export default function Customer() {
               }}
               noValidate
               autoComplete="off"
-            > <ThemeProvider theme={theme}>
-            <Box
-              sx={{
-                bgcolor: "background.paper",
-                boxShadow: 2,
-                borderRadius: 2,
-                p: 3,
-                minWidth: 280,
-              }}
             >
-              <Box sx={{ color: "#ecf0f1",bgcolor:'#e74c3c',pt:1.5,pb:1.5,pr:2,pl:2,borderRadius: 2,textAlign:'center',fontWeight:'bold',fontFamily:'Poppines', fontSize: 20 }}>Search Customer</Box>
-              <Box
-                sx={{
-                  mt:3,
-                  color: "text.primary",
-                  fontSize: 34,
-                  fontWeight: "medium",
-                  textAlign:'center'
-                }}
-              >
-               <TextField
-                id="outlined-basic"
-                label="Outlined"
-                variant="outlined"
-              />
-              </Box>
-             
-            </Box>
-          </ThemeProvider>
-              
+              {" "}
+              <ThemeProvider theme={theme}>
+                <Box
+                  sx={{
+                    bgcolor: "background.paper",
+                    boxShadow: 2,
+                    borderRadius: 2,
+                    p: 3,
+                    minWidth: 280,
+                  }}
+                >
+                  <Box
+                    sx={{
+                      color: "#ecf0f1",
+                      bgcolor: "#e74c3c",
+                      pt: 1.5,
+                      pb: 1.5,
+                      pr: 2,
+                      pl: 2,
+                      borderRadius: 2,
+                      textAlign: "center",
+                      fontWeight: "bold",
+                      fontFamily: "Poppines",
+                      fontSize: 22,
+                    }}
+                  >
+                    Manage Customer
+                  </Box>
+                  <Box
+                    sx={{
+                      mt: 3,
+                      color: "text.primary",
+                      fontSize: 34,
+                      fontWeight: "medium",
+                      textAlign: "center",
+                    }}
+                  >
+                    <TextField
+                      id="outlined-basic"
+                      label="Outlined"
+                      variant="outlined"
+                    />
+                  </Box>
+                </Box>
+              </ThemeProvider>
             </Box>
           </Grid>
         </Grid>
